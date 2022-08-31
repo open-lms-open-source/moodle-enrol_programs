a5a12502d8cd25b5ca879b4d9c6b5800ee231dae
Petr Skoda <petr.skoda@openlms.net>
2022-08-31 10:11:55 +0200

    OWORK-295 sync version to OLMS Work

--- version.php
+++ version.php
@@ -27,11 +27,11 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 
-$plugin->version   = 2022081401;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2022083100;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2021051704;        // Requires this Moodle version.
 $plugin->component = 'enrol_programs';
-$plugin->maturity  = MATURITY_ALPHA;
-$plugin->release   = '1.0.0-alpha3+';
+$plugin->maturity  = MATURITY_RC;
+$plugin->release   = 'v1.0.6.2+';
 $plugin->supported = [311, 400];
 
-$plugin->dependencies = ['local_openlms' => 2022053100];
+$plugin->dependencies = ['local_openlms' => 2022083100];
