62da5d8688dc80b5cfe051a8d7ed55251b05312c
Petr Skoda <petr.skoda@openlms.net>
2022-11-15 12:56:36 +0100

    OWORK-384 license cleanup

--- db/services.php
+++ db/services.php
@@ -20,7 +20,7 @@ defined('MOODLE_INTERNAL') || die();
  * Program enrolment external functions.
  *
  * @package    enrol_programs
- * @copyright  Copyright (c] 2022 Open LMS (https://www.openlms.net/]
+ * @copyright  2022 Open LMS (https://www.openlms.net/)
  * @author     Petr Skoda
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
--- version.php
+++ version.php
@@ -27,11 +27,11 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 
-$plugin->version   = 2022103100;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2022111500;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2021051704;        // Requires this Moodle version.
 $plugin->component = 'enrol_programs';
 $plugin->maturity  = MATURITY_RC;
 $plugin->release   = 'v1.0.7.1+';
 $plugin->supported = [311, 400];
 
-$plugin->dependencies = ['local_openlms' => 2022103100];
+$plugin->dependencies = ['local_openlms' => 2022111500];
