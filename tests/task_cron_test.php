192d0adbdac9f854c6d80add189a65fe9ca8360b
Petr Skoda <petr.skoda@openlms.net>
2023-06-19 12:28:17 +0200

    OWORK-616 improve cron tests

--- tests/task_cron_test.php
+++ tests/task_cron_test.php
@@ -16,7 +16,7 @@
 
 namespace enrol_programs;
 
-use enrol_programs\task\cron;
+use enrol_programs\local\program;
 
 /**
  * Programs cron test.
@@ -46,13 +46,39 @@ final class task_cron_test extends \advanced_testcase {
 
         $user1 = $this->getDataGenerator()->create_user();
         $user2 = $this->getDataGenerator()->create_user();
+        $user3 = $this->getDataGenerator()->create_user();
 
-        $program1 = $generator->create_program(['sources' => ['manual' => []]]);
+        $course1 = $this->getDataGenerator()->create_course();
+        $context1 = \context_course::instance($course1->id);
+        $course2 = $this->getDataGenerator()->create_course();
+        $context2 = \context_course::instance($course2->id);
+        $course3 = $this->getDataGenerator()->create_course();
+        $context3 = \context_course::instance($course3->id);
+        $course4 = $this->getDataGenerator()->create_course();
+        $context4 = \context_course::instance($course4->id);
+
+        $program1 = $generator->create_program(['sources' => ['manual' => []], 'creategroups' => 1]);
         $source1 = $DB->get_record('enrol_programs_sources', ['programid' => $program1->id, 'type' => 'manual'], '*', MUST_EXIST);
+        $top1 = program::load_content($program1->id);
+        $top1->append_course($top1, $course1->id);
+        $top1->append_course($top1, $course2->id);
+        $top1->append_course($top1, $course3->id);
+        \enrol_programs\local\source\manual::allocate_users($program1->id, $source1->id, [$user1->id, $user2->id]);
 
         $program2 = $generator->create_program(['sources' => ['manual' => []]]);
 
-        \enrol_programs\local\source\manual::allocate_users($program1->id, $source1->id, [$user1->id, $user2->id]);
+        $this->assertTrue(is_enrolled($context1, $user1, '', true));
+        $this->assertTrue(is_enrolled($context2, $user1, '', true));
+        $this->assertTrue(is_enrolled($context3, $user1, '', true));
+
+        $candidates = \enrol_programs\local\notification_manager::get_candidate_types($program1->id);
+        foreach ($candidates as $type => $name) {
+            $generator->create_program_notification(['programid' => $program1->id, 'notificationtype' => $type]);
+        }
+
+        delete_course($course3->id, false);
+        delete_user($user2);
+        \enrol_programs\local\source\manual::allocate_users($program1->id, $source1->id, [$user3->id]);
 
         // Just make sure there are no obvious errors.
         $this->setAdminUser();
